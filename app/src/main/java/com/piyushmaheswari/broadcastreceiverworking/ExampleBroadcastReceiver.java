3fb50495ac8b487201ee49b931bc68c5b5167150
Piyush2769 <startcheckingyourmail@gmail.com>
2019-11-24 19:45:39 +0530

    Static Broadcast Done.

--- app/src/main/java/com/piyushmaheswari/broadcastreceiverworking/ExampleBroadcastReceiver.java
+++ app/src/main/java/com/piyushmaheswari/broadcastreceiverworking/ExampleBroadcastReceiver.java
@@ -3,11 +3,23 @@ package com.piyushmaheswari.broadcastreceiverworking;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.widget.Toast;
 
 public class ExampleBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+
+        if(Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction()))
+        {
+            Toast.makeText(context, "Boot Completed", Toast.LENGTH_SHORT).show();
+        }
+
+        if(ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction()))
+        {
+            Toast.makeText(context, "Connection Changed", Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
